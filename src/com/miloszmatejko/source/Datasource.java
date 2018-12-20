e7611f361ec562660cd98471848fb7a0581daef6
Milosz <milosz.matejko@gmail.com>
2018-12-20 22:52:17 +0100

    Made classes Book and Book_genre. Defined fields in datasource, listed methods.

--- src/com/miloszmatejko/source/Datasource.java
+++ src/com/miloszmatejko/source/Datasource.java
@@ -1,7 +1,12 @@
 package com.miloszmatejko.source;
 
+import com.miloszmatejko.common.Book_genre;
+import com.miloszmatejko.common.Book;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
 
@@ -9,8 +14,50 @@ public class Datasource {
     public static final String DB_NAME = "database.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\milosz\\projects\\java-sqltraining\\database.db";
 
+
+    public static final String TABLE_BOOK_GENRES = "book_genres";
+    public static final String COLUMN_GENRE_ID = "genre_id";
+    public static final String COLUMN_GENRE_NAME = "genre_name";
+
+    public static final String TABLE_BOOKS = "books";
+    public static final String COLUMN_BOOK_ISBN = "book_ISBN";
+    public static final String COLUMN_BOOK_NAME = "book_name";
+    public static final String COLUMN_GENRE = "genre";
+
+
     private Connection connection;
 
-    private PreparedStatement insertNewGenre;
-    private PreparedStatement insertNewBook;
+
+    private PreparedStatement insertIntoGenres;
+    private PreparedStatement insertIntoBooks;
+    private PreparedStatement queryGenre;
+    private PreparedStatement queryBook;
+
+
+    private List<Book_genre> showAllGenres() {
+
+        List<Book_genre> genres = new ArrayList<> ();
+
+        return genres;
+    }
+
+    private List<Book> queryBooksOfGenre(String genre) {
+        List<Book> books = new ArrayList<> ();
+        return books;
+    }
+
+
+
+    private int insertGenre(String name) {
+        return 0;
+    }
+
+    private void insertBook(String name, String genre) {
+
+    }
+
+    private void updateData(String bookName, String newIsbn, String newBookName, String genre) {
+
+        //genre id is gonna be returned from insertGenre
+    }
 }
