3a4d7166aa593dda2a48ea1e57af3baa608b9e34
Milosz <milosz.matejko@gmail.com>
2018-12-29 17:35:44 +0100

    Finished adding methods.

--- src/com/miloszmatejko/Main.java
+++ src/com/miloszmatejko/Main.java
@@ -1,43 +1,45 @@
 package com.miloszmatejko;
 
-import com.miloszmatejko.common.BookOfGenre;
-import com.miloszmatejko.common.Genre;
-import com.miloszmatejko.source.Datasource;
+import com.miloszmatejko.source.DataSource;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Datasource datasource = new Datasource ();
+        DataSource datasource = new DataSource ();
 
-        System.out.println ("----querying all genres-----");
-
-        List<Genre> genres = datasource.queryGenres ();
-        if (genres == null) {
-            System.out.println ( "no genres" );
-            return;
-        }
-
-        for (Genre genre : genres) {
-            System.out.println (genre.toString ());
-        }
-
-
-        System.out.println ("-----------------querying all books of one genre--------");
-        List<BookOfGenre> booksOfGenre = datasource.queryBooksOfGenre ( "Horror" );
-
-        for (BookOfGenre bookOfGenre : booksOfGenre) {
-            System.out.println (bookOfGenre.toString ());
-        }
+//        System.out.println ("----querying all genres-----");
+//
+//        List<Genre> genres = datasource.queryGenres ();
+//        if (genres == null) {
+//            System.out.println ( "no genres" );
+//            return;
+//        }
+//
+//        for (Genre genre : genres) {
+//            System.out.println (genre.toString ());
+//        }
 
 
-        System.out.println ( "-----------------inserting a new Genre-------------------" );
+//        System.out.println ("-----------------querying all books of one genre--------");
+//        List<BookOfGenre> booksOfGenre = datasource.queryBooksOfGenre ( "Horror" );
+//
+//        for (BookOfGenre bookOfGenre : booksOfGenre) {
+//            System.out.println (bookOfGenre.toString ());
+//        }
+
+
+//        System.out.println ( "-----------------inserting a new Genre-------------------" );
 
 //        System.out.println ( (datasource.insertIntoGenres ( "Romance" )) );
 
-    datasource.inserIntoBooks ( "123456789123", "Peppa Pig", "Scholastic"  );
+//    datasource.insertIntoBooks ( "9375847592834", "Harry Potter", "Fantasy"  );
+
+//        System.out.println ( "-------------------------updating a book----------------------" );
+//        datasource.updateBook ( "The Shining", "2919191919191", "Sherlock Holmes", "Crime Story" );
+
+        datasource.deleteFromBooks ( "Harry Potter" );
     }
 
 
--- src/com/miloszmatejko/source/DataSource.java
+++ src/com/miloszmatejko/source/DataSource.java
@@ -7,44 +7,48 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Datasource {
+public class DataSource {
 
 
-    public static final String DB_NAME = "database.db";
-    public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
+    private static final String DB_NAME = "database.db";
+    private static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
 
 
-    public static final String TABLE_GENRES = "genres";
-    public static final String COLUMN_GENRE_ID = "genre_id";
-    public static final String COLUMN_GENRE_NAME = "name";
+    private static final String TABLE_GENRES = "genres";
+    private static final String COLUMN_GENRE_ID = "genre_id";
+    private static final String COLUMN_GENRE_NAME = "name";
 
-    public static final String TABLE_BOOKS = "books";
-    public static final String COLUMN_BOOK_ISBN = "ISBN";
-    public static final String COLUMN_BOOK_TITLE = "title";
-    public static final String COLUMN_BOOK_GENRE = "genre";
+    private static final String TABLE_BOOKS = "books";
+    private static final String COLUMN_BOOK_ISBN = "ISBN";
+    private static final String COLUMN_BOOK_TITLE = "title";
+    private static final String COLUMN_BOOK_GENRE = "genre";
 
-    public static final String QUERY_GENRES = "SELECT * FROM " + TABLE_GENRES;
+    private static final String QUERY_GENRES = "SELECT * FROM " + TABLE_GENRES;
 
-    public static final String TABLE_BOOK_OF_GENRE_VIEW = "books_of_genre";
+    private static final String TABLE_BOOK_OF_GENRE_VIEW = "books_of_genre";
 
-    public static final String CREATE_BOOK_OF_GENRE_VIEW = "CREATE VIEW IF NOT EXISTS " +
+    private static final String CREATE_BOOK_OF_GENRE_VIEW = "CREATE VIEW IF NOT EXISTS " +
             TABLE_BOOK_OF_GENRE_VIEW + " AS SELECT " + TABLE_GENRES + "." + COLUMN_GENRE_NAME + ", " +
             TABLE_BOOKS + "." + COLUMN_BOOK_ISBN + ", " + TABLE_BOOKS + "." + COLUMN_BOOK_TITLE +
             " FROM " + TABLE_GENRES + " INNER JOIN " + TABLE_BOOKS + " ON " + TABLE_BOOKS + "." + COLUMN_BOOK_GENRE +
             " = " + TABLE_GENRES + "." + COLUMN_GENRE_ID;
 
-    public static final String BOOK_OF_GENRE_VIEW_PREP = "SELECT * FROM " + TABLE_BOOK_OF_GENRE_VIEW + " WHERE " +
+    private static final String BOOK_OF_GENRE_VIEW_PREP = "SELECT * FROM " + TABLE_BOOK_OF_GENRE_VIEW + " WHERE " +
             COLUMN_BOOK_GENRE + " = ?";
 
-    public static final String QUERY_GENRE = "SELECT " + COLUMN_GENRE_ID + " FROM " + TABLE_GENRES +
+    private static final String QUERY_GENRE = "SELECT " + COLUMN_GENRE_ID + " FROM " + TABLE_GENRES +
             " WHERE " + COLUMN_GENRE_NAME + " = ?";
 
-    public static final String INSERT_GENRES = "INSERT INTO " + TABLE_GENRES +
+    private static final String INSERT_GENRES = "INSERT INTO " + TABLE_GENRES +
             '(' + COLUMN_GENRE_NAME + ") VALUES (?)";
 
-    public static final String INSERT_BOOKS = "INSERT INTO " + TABLE_BOOKS +
+    private static final String INSERT_BOOKS = "INSERT INTO " + TABLE_BOOKS +
             '(' + COLUMN_BOOK_ISBN + ", " + COLUMN_BOOK_TITLE + ", " + COLUMN_BOOK_GENRE + ") VALUES(?, ?, ?)";
 
+    private static final String UPDATE_BOOKS = "UPDATE " + TABLE_BOOKS + " SET " + COLUMN_BOOK_ISBN + " = ?, " +
+            COLUMN_BOOK_TITLE + " = ?, " + COLUMN_BOOK_GENRE +
+            " = ? WHERE " + COLUMN_BOOK_TITLE + " = ?";
+    private static final String DELETE_BOOKS = "DELETE FROM " + TABLE_BOOKS + " WHERE "+ COLUMN_BOOK_TITLE +" = ?";
 
     public List<Genre> queryGenres() {
 
@@ -176,7 +180,7 @@ public class Datasource {
         }
     }
 
-    public void inserIntoBooks(String isbn, String name, String genre) {
+    public void insertIntoBooks(String isbn, String name, String genre){
         Connection connection = null;
         PreparedStatement insertIntoBooks = null;
         try {
@@ -217,8 +221,79 @@ public class Datasource {
 
     }
 
-    private void updateData(String bookName, String newIsbn, String newBookName, String genre) {
+    public void updateBook(String bookOldName, String newIsbn, String newBookTitle, String newGenre) {
 
-        //genre id is gonna be returned from insertGenre?
+        Connection connection = null;
+        PreparedStatement updateBook = null;
+
+        try {
+            connection = DriverManager.getConnection ( CONNECTION_STRING );
+            connection.setAutoCommit ( false );
+            updateBook = connection.prepareStatement ( UPDATE_BOOKS );
+            System.out.println ("updateBook = " + updateBook.toString ());
+            updateBook.setString ( 1, newIsbn );
+            updateBook.setString ( 2, newBookTitle );
+            int genreId = insertIntoGenres ( newGenre );
+            updateBook.setInt ( 3, genreId );
+            updateBook.setString ( 4, bookOldName );
+            System.out.println ("updateBook after setString" + updateBook.toString ());
+            int affectedRows  = updateBook.executeUpdate ();
+            System.out.println ("affected rows = "+ affectedRows);
+            if (affectedRows == 1) {
+                connection.commit ();
+            } else {
+                throw new SQLException ( "update failed" );
+            }
+        } catch (SQLException e ) {
+            try {
+                System.out.println ( "Exception in updateBook " + e.getMessage () );
+                connection.rollback ();
+            } catch (SQLException e1) {
+                System.out.println ("couldn't perform a rollback" + e.getMessage ());
+            }
+        }finally {
+            try {
+                System.out.println ("setting autocommit default true ");
+                    connection.setAutoCommit ( true );
+                if (updateBook != null) {
+                    updateBook.close ();
+                }
+                if (connection!=null) {
+                    connection.close ();
+                }
+            } catch (SQLException e) {
+                System.out.println ( "setting autocommit true failed" + e.getMessage () );
+            }
+        }
     }
+
+    public void deleteFromBooks(String bookTitle) throws SQLException {
+        Connection connection = null;
+        PreparedStatement deleteFromBooks = null;
+
+        try {
+            connection = DriverManager.getConnection ( CONNECTION_STRING );
+            deleteFromBooks = connection.prepareStatement ( DELETE_BOOKS );
+            deleteFromBooks.setString ( 1, bookTitle );
+            int affectedRows = deleteFromBooks.executeUpdate ();
+            if (affectedRows <1) {
+                throw new SQLException ( "Deleted Nothing!" );
+            }
+        } catch (SQLException e) {
+            System.out.println ( "Exception in deleteFromBooks " + e.getMessage () );
+        }finally {
+            try {
+                if (deleteFromBooks != null) {
+                    deleteFromBooks.close ();
+                }
+                if (connection != null) {
+                    connection.close ();
+                }
+            } catch (SQLException e) {
+                System.out.println ("unable to close something after deleteFromBooks" + e.getMessage ());
+            }
+        }
+
+    }
+
 }
